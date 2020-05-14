fbe9a88d0531d7d34ca77f66dd1c184254fbb915
Vladimir Kapustin <Vladimir-Kapustin@users.noreply.github.com>
2020-05-14 23:31:07 +0300

    homework for stream 19.04.20

--- JSProCode_Stream2_HomeWork/main.js
+++ JSProCode_Stream2_HomeWork/main.js
@@ -1,34 +1,43 @@
 //Есть некий массив с элементами. На основе него вы формируете список, примерно, как делали список статей. Когда наводим мышку на элемент массива, в конце статьи появлялся крестик. Когда вы на этот крестик кликаете, соответственно этот элемент (статья) должен исчезать.
 
-let arr = [
+let tasks = [
   {
-    shortDescription: "",
-    fullDescription: "",
-    owner: "",
+    shortDescription: "Task1",
+    fullDescription: "Description1",
+    owner: "Vladimir",
   },
   {
-    shortDescription: "",
-    fullDescription: "",
-    owner: "",
+    shortDescription: "Task2",
+    fullDescription: "Description2",
+    owner: "Vladimir",
   },
   {
-    shortDescription: "",
-    fullDescription: "",
-    owner: "",
+    shortDescription: "Task3",
+    fullDescription: "Description3",
+    owner: "Vladimir",
   },
   {
-    shortDescription: "",
-    fullDescription: "",
-    owner: "",
+    shortDescription: "Task4",
+    fullDescription: "Description4",
+    owner: "Vladimir",
   },
   {
-    shortDescription: "",
-    fullDescription: "",
-    owner: "",
+    shortDescription: "Task5",
+    fullDescription: "Description5",
+    owner: "Vladimir",
   },
   {
-    shortDescription: "",
-    fullDescription: "",
-    owner: "",
+    shortDescription: "Task6",
+    fullDescription: "Description6",
+    owner: "Vladimir",
   },
 ];
+
+let str = "";
+for (let i = 0; i < tasks.length; i++) {
+  str = `${str}<li class="sublist${i}">${tasks[i].shortDescription}</li>`;
+  document.querySelector(`.sublist${i}`).addEventListener;
+}
+
+const a = document.querySelector(".list1");
+a.innerHTML = str;
