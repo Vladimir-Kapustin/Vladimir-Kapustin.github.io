ed5ba48335ecb49d199c53ba52bd8ffc35424190
Vladimir Kapustin <Vladimir-Kapustin@users.noreply.github.com>
2020-07-03 00:55:14 +0300

    changeFolderNames

---
+++ 2020-07-02 JavaScript-dev-acquaintance/main.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+  const result = a + b;
+
+  return result;
+}
+
+const a = 6;
+const b = 7;
+
+const result = sum(a, b);
+
+console.log("the sum of numbers: " + a + " and " + b + " = " + result);
+
+// const btn = document.getElementById("btn");
+// const time = document.getElementById("time");
+
+// btn.onclick = function () {
+//   console.log("click");
+//   time.innerHTML = new Date().toLocaleTimeString();
+// };
